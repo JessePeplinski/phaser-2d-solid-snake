e8f49fe4bafccc038fd91fa482253bebc48065c2
Jesse Peplinski <peplinski.jesse@gmail.com>
2025-02-27 16:30:49 -0500

    Get simple 2d game working

--- src/main.js
+++ src/main.js
@@ -1,20 +1,28 @@
+import { TilemapGame } from './scenes/TilemapGame';
 import { Game as MainGame } from './scenes/Game';
-import { AUTO, Scale,Game } from 'phaser';
+import { AUTO, Scale, Game, Physics } from 'phaser';
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config = {
     type: AUTO,
-    width: 1024,
-    height: 768,
+    width: 800,
+    height: 600,
     parent: 'game-container',
-    backgroundColor: '#028af8',
+    backgroundColor: '#2d2d2d',
+    pixelArt: true,
     scale: {
         mode: Scale.FIT,
         autoCenter: Scale.CENTER_BOTH
     },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 }
+        }
+    },
     scene: [
-        MainGame
+        TilemapGame
     ]
 };
 
