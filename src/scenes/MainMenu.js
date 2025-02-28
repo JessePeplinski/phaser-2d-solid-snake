d7b3d8810421eefcee69eaee2791668f9ba02035
Jesse Peplinski <peplinski.jesse@gmail.com>
2025-02-28 11:25:01 -0500

    Update peptech URL and phaser js url

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -22,7 +22,7 @@ export class MainMenu extends Scene {
             align: 'center'
         }).setOrigin(0.5);
 
-        this.add.text(centerX, 250, 'A simple browser based 2D metal gear solid game.', {
+        this.add.text(centerX, 250, 'A simple browser based 2D metal gear solid game', {
             fontFamily: 'Arial Black',
             fontSize: 24,
             color: '#ffffff',
@@ -31,7 +31,7 @@ export class MainMenu extends Scene {
             align: 'center'
         }).setOrigin(0.5);
 
-        // Split the credit text into parts so that only "@peptech" is clickable.
+        // Create interactive credit text with clickable parts.
         const creditStyle = {
             fontFamily: 'Arial Black',
             fontSize: 24,
@@ -41,12 +41,14 @@ export class MainMenu extends Scene {
             align: 'center'
         };
 
-        // Create the individual text objects
+        // Split the text into several parts:
+        // "Created by " + "@peptech" + " with " + "Phaser.js" + "."
         const textCreatedBy = this.add.text(0, 0, 'Created by ', creditStyle);
         const textPeptech = this.add.text(textCreatedBy.width, 0, '@peptech', creditStyle);
-        const textWith = this.add.text(textCreatedBy.width + textPeptech.width, 0, ' with Phaser.js.', creditStyle);
+        const textWith = this.add.text(textCreatedBy.width + textPeptech.width, 0, ' with ', creditStyle);
+        const textPhaser = this.add.text(textCreatedBy.width + textPeptech.width + textWith.width, 0, 'Phaser.js', creditStyle);
 
-        // Make the "@peptech" text interactive
+        // Make the "@peptech" text interactive.
         textPeptech.setInteractive({ useHandCursor: true });
         textPeptech.on('pointerover', () => {
             textPeptech.setStyle({ fill: '#f39c12' });
@@ -55,18 +57,31 @@ export class MainMenu extends Scene {
             textPeptech.setStyle({ fill: '#ffffff' });
         });
         textPeptech.on('pointerdown', () => {
-            window.open('https://twitter.com/peptech', '_blank');
+            window.open('https://x.com/peptech', '_blank');
         });
 
-        // Calculate total width and center the container horizontally
-        const totalWidth = textCreatedBy.width + textPeptech.width + textWith.width;
+        // Make the "Phaser.js" text interactive.
+        textPhaser.setInteractive({ useHandCursor: true });
+        textPhaser.on('pointerover', () => {
+            textPhaser.setStyle({ fill: '#f39c12' });
+        });
+        textPhaser.on('pointerout', () => {
+            textPhaser.setStyle({ fill: '#ffffff' });
+        });
+        textPhaser.on('pointerdown', () => {
+            window.open('https://phaser.io', '_blank');
+        });
+
+        // Calculate total width and center the container.
+        const totalWidth = textCreatedBy.width + textPeptech.width + textWith.width + textPhaser.width
         const creditContainer = this.add.container(centerX - totalWidth / 2, 300, [
             textCreatedBy,
             textPeptech,
-            textWith
+            textWith,
+            textPhaser
         ]);
 
-        // Create the Start Game button as interactive text
+        // Create the Start Game button as interactive text.
         const startButton = this.add.text(centerX, 400, 'Start Game', {
             fontFamily: 'Arial Black',
             fontSize: 38,
