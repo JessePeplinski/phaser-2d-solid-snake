dfc9aaf90b7285d40d5f97fbae3111b236a9f227
Jesse Peplinski <peplinski.jesse@gmail.com>
2025-02-28 10:40:32 -0500

    Update main menu screen

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -11,9 +11,22 @@ export class MainMenu extends Scene
     {
         this.add.image(512, 384, 'background');
 
-        this.add.image(512, 300, 'logo');
+        // this.add.image(512, 300, 'logo');
 
-        this.add.text(512, 460, 'Main Menu', {
+        this.add.text(412, 200, 'Solid Snek', {
+            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
+            stroke: '#000000', strokeThickness: 8,
+            align: 'center'
+        }).setOrigin(0.5);
+
+        this.add.text(412, 300, 'A simple browser based 2D metal gear solid game.', {
+            fontFamily: 'Arial Black', fontSize: 24, color: '#ffffff',
+            stroke: '#000000', strokeThickness: 8,
+            align: 'center'
+        }).setOrigin(0.5);
+
+
+        this.add.text(412, 400, 'Start Game', {
             fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
             align: 'center'
