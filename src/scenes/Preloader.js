15633aaf7b505d04affc8f714b4f3361f6209db8
Jesse Peplinski <peplinski.jesse@gmail.com>
2025-03-07 17:32:03 -0500

    update level select list

--- src/scenes/Preloader.js
+++ src/scenes/Preloader.js
@@ -65,11 +65,11 @@ export class Preloader extends Scene
         
         // --- TILEMAPS ---
         // this.load.tilemapCSV('map', 'tilemaps/catastrophi_level2.csv');
-        this.load.tilemapCSV('level0', 'tilemaps/catastrophi_level2.csv');
+        // this.load.tilemapCSV('level0', 'tilemaps/catastrophi_level2.csv');
         this.load.tilemapCSV('level1', 'tilemaps/level1.csv');
         this.load.tilemapCSV('level2', 'tilemaps/level2.csv');
         this.load.tilemapCSV('level3', 'tilemaps/level3.csv');
-        this.load.tilemapCSV('level4', 'tilemaps/level4.csv');
+        // this.load.tilemapCSV('level4', 'tilemaps/level4.csv');
         
         // --- AUDIO ---
         // Music
