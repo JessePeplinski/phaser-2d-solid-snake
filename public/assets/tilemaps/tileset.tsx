74a4ec3b675149aa8bc159412118f34a814d578d
Jesse Peplinski <peplinski.jesse@gmail.com>
2025-03-14 16:06:35 -0400

    udpate tileset to have custom properties

--- public/assets/tilemaps/tileset.tsx
+++ public/assets/tilemaps/tileset.tsx
@@ -1,4 +1,27 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="tileset" tilewidth="16" tileheight="16" tilecount="168" columns="28">
+ <editorsettings>
+  <export target="tileset.tsx" format="tsx"/>
+ </editorsettings>
  <image source="catastrophi_tiles_16.png" width="448" height="96"/>
+ <tile id="140">
+  <properties>
+   <property name="exit" value="true"/>
+  </properties>
+ </tile>
+ <tile id="141">
+  <properties>
+   <property name="playerspawn" value="true"/>
+  </properties>
+ </tile>
+ <tile id="142">
+  <properties>
+   <property name="aispawn" value="true"/>
+  </properties>
+ </tile>
+ <tile id="143">
+  <properties>
+   <property name="aipath" value="true"/>
+  </properties>
+ </tile>
 </tileset>
